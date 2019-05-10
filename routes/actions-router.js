e6c73ba50698ae11fe78f56fbb26b88e18d0184c
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-05-10 12:52:50 -0400

    created put and delete for actions and projects, all working

--- routes/actions-router.js
+++ routes/actions-router.js
@@ -38,4 +38,42 @@ router.post('/', (req, res) => {
   })
 })
 
+//UPDATE ACTIONS
+//WORKING
+router.put('/:id', (req, res) => {
+  db('actions')
+  .where({ id: req.params.id})
+  .update(req.body)
+  .then(action => {
+      if(action === 0) {
+          res.status(404).json({ message: 'The action associated with this id cannot be found' }); 
+      } else {
+          res.status(201).json(action)
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ error: err, message: 'There was an error updating the action data'})
+  })
+})
+
+//DELETE actions
+//WORKING
+router.delete('/:id', (req, res) => {
+  db('actions')
+  .where({ id: req.params.id})
+  .del()
+  .then(action => {
+      if(action === 1) {
+          res.status(200).end()
+      }
+  })
+  .catch(error => {
+      res.status(500).json({ error: err, message: 'There was an error deleting the project data'})
+  })
+})
+
+
+
+
+
 module.exports = router;
--- routes/projects-router.js
+++ routes/projects-router.js
@@ -42,6 +42,14 @@ router.post('/', (req, res) => {
 
 
  //Get projects by id and return actions with it 
+ //working
+ //  .where({id: req.params.id}) wouldn't work...had to deconstruct it and try that way...weird
+ //
+ // 
+ //projects is returning actions array
+ //project_id is equal to id on the projects table
+ //calling it projects.actions = actions like a join where you would say cohort.cohort_id = id 
+
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   db('projects')
@@ -61,6 +69,46 @@ router.get('/:id', (req, res) => {
       //id is not defined 
  })
 })
+
+//UPDATE Projects
+//Working
+router.put('/:id', (req, res) => {
+  db('projects')
+  .where({ id: req.params.id})
+  .update(req.body)
+  .then(project => {
+      if(project === 0) {
+          res.status(404).json({ message: 'The projects associated with this id cannot be found' }); 
+      } else {
+          res.status(201).json(project)
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ error: err, message: 'There was an error updating the project data'})
+  })
+})
+
+
+//DELETE projects
+//WORKING 
+router.delete('/:id', (req, res) => {
+  db('projects')
+  .where({ id: req.params.id})
+  .del()
+  .then(project => {
+      if(project === 1) {
+          res.status(200).end()
+      }
+  })
+  .catch(error => {
+      res.status(500).json({ error: err, message: 'There was an error deleting the project data'})
+  })
+})
+
+
+
+
+
 
 
 
